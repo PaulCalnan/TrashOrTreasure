1fc2e7582429ffe17d453a6b68e5ffd8523ac874
Paul Calnan <pcalnan71@gmail.com>
2021-05-01 05:24:37 +1000

    constrained image size on show

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:new, :create]
   resources :posts, only: [:new, :create, :index, :show, :destroy] do
-    resources :comments, only: [:new, :create, :show, :destroy]
+    resources :comments, only: [:new, :create, :show, :destroy] # nested routes to allow ID comparisons on show page
   end
 
-
-  #get "/index" => "posts#index"
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
